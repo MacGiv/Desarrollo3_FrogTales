e4db08c3288f3141609582f8b3dbbd639b2edeb5
MacGiv <tomasluchelli.17@gmail.com>
2026-09-15 08:15:39 -0300

    Add player idle state

---
+++ Assets/Scripts/Player States/PlayerIdleState.cs
@@ -0,0 +1,37 @@
+using FrogGame.Core;
+
+namespace FrogGame.Gameplay
+{
+    public class PlayerIdleState : IState
+    {
+        private readonly PlayerBrain brain;
+
+        public PlayerIdleState(PlayerBrain brain) => this.brain = brain;
+
+        public void Enter() => brain.MovementHandler.Stop();
+
+        public void LogicUpdate()
+        {
+            if (brain.InputHandler.TonguePressed)
+            {
+                brain.FSM.ChangeState(brain.TongueState);
+                return;
+            }
+
+            if (brain.InputHandler.ArrowPressed)
+            {
+                brain.FSM.ChangeState(brain.ArrowState);
+                return;
+            }
+
+            if (brain.InputHandler.MoveInput != UnityEngine.Vector2.zero)
+            {
+                brain.FSM.ChangeState(brain.MoveState);
+            }
+        }
+
+        public void PhysicsUpdate() { }
+        public void Exit() { }
+    }
+}
+
